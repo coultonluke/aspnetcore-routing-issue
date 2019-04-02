f02cc98dab47450ca3d7f385cfadacf0258b0734
Luke Coulton <luke.coulton@ao.com>
2019-04-02 16:10:19 +0100

    Initial commit

---
+++ WebApplication4/Controllers/QuizController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class QuizController : Controller
+    {
+        public async Task<IActionResult> Start()
+        {
+            return await Task.FromResult(this.View());
+        }
+
+        public async Task<IActionResult> Question(string urlId, int? questionNumber)
+        {
+            const int actualQuestionNumber = 2;
+            
+            if (questionNumber != null && questionNumber.Value != actualQuestionNumber)
+            {
+                // Redirect to question 2
+                return await Task.FromResult(this.RedirectToRoute("Quiz", new { urlId = urlId, questionNumber = actualQuestionNumber }));
+            }
+
+            return await Task.FromResult(this.View());
+        }
+    }
+}
